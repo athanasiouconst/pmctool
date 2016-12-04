4cc7070224c744b52e5a3e5dfbe6ce8598d3c0eb
costas <costas@DESKTOP-GB1ASLH>
2016-12-04 19:01:32 +0200

    Revert "commit pmctool version 2. Model, Complexity Factor, PDF"
    This reverts commit 310c6f8613af2a47f6b775fc119c56d155e2ed1c.

--- application/views/footer/pmctoolFooter.php
+++ application/views/footer/pmctoolFooter.php
@@ -1,19 +1,31 @@
 <footer class="footer-section">
             <div class="container">
                 <div class="row mt-30 mb-30">
-                    
-                    <div class="col-md-12 text-center">
+                    <div class="col-md-6 text-center">
                         <!-- Footer Copy Right Text -->
                         <div class="copyright-info">
-                            <a href="https://www.linkedin.com/in/constantinosathanasiou" target="_blank"><span><i class="fa fa-code"></i></span>The PMCTool has been Developed  <span><i class="fa fa-code"></i></span><br> By <span>Constantinos Athanasiou</span></a>
+                            <a href="http://themebite.com"><span><i class="fa fa-code"></i></span> The Theme has been Developed  <span><i class="fa fa-heart"></i></span> <br>By <span>ThemeBite</span></a>
+                        </div>
+                        <!-- Footer Social Icons -->
+                        <div class="social-icons mt-30">
+                            <a href="https://www.facebook.com/themebite/"><i class="fa fa-facebook"></i></a>
+                            <a href="https://twitter.com/themebite/"><i class="fa fa-twitter"></i></a>
+                            <a href="https://plus.google.com/themebite/"><i class="fa fa-google-plus"></i></a>
+                            <a href="https://github.com/themebite/"><i class="fa fa-github"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <!-- Footer Copy Right Text -->
+                        <div class="copyright-info">
+                            <a href="http://themebite.com"><span><i class="fa fa-code"></i></span> The PMCTool has been Developed  <span><i class="fa fa-heart"></i></span> <br>By <span>Constantinos Athanasiou</span></a>
                         </div>
 
                         <!-- Footer Social Icons -->
                         <div class="social-icons mt-30">
-                            <a href="https://www.facebook.com/costas.siou/" target="_blank"><i class="fa fa-facebook"></i></a>
-                            <a href="https://twitter.com/csiouath/" target="_blank"><i class="fa fa-twitter"></i></a>
-                            <a href="https://plus.google.com/u/0/110129558289042781457/" target="_blank"><i class="fa fa-google-plus"></i></a>
-                            <a href="https://github.com/athanasiouconst/" target="_blank"><i class="fa fa-github"></i></a>
+                            <a href="https://www.facebook.com/themebite/"><i class="fa fa-facebook"></i></a>
+                            <a href="https://twitter.com/themebite/"><i class="fa fa-twitter"></i></a>
+                            <a href="https://plus.google.com/themebite/"><i class="fa fa-google-plus"></i></a>
+                            <a href="https://github.com/themebite/"><i class="fa fa-github"></i></a>
                         </div>
                     </div>
                 </div>
